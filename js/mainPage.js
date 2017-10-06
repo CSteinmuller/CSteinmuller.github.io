3b34ae8723f5ec7355c5dc5f7f34702181a03434
Mackenzie <ma.luzarow@gmail.com>
2017-10-05 21:27:44 -0500

    Changed the way page tiling works.
    Pages now tile on top of each other instead of expanding and shrinking.

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,9 +1,10 @@
 var lastScrollOrigin = 0;
-var pages = new Array ();
 var screenHeight = window.innerHeight.toString () + "px";
 var screenWidth = window.innerWidth.toString () + "px";
-var pageIndex = 1;
+var pageIndex = 0;
 var muteWheelListener = false;
+var codeBox;
+var pages;
 
 /******************************************************************************************
 **
@@ -13,41 +14,38 @@ var muteWheelListener = false;
 window.onload = function () {
     // Initialize page element variables
     codeBox = document.getElementById ("frame_background");
-    pages[1] = document.getElementById ("page_01");
-    pages[2] = document.getElementById ("page_02");
-    pages[3] = document.getElementById ("page_03");
-    
-    pages[1].style.height = screenHeight;
-    pages[2].style.height = "0px";
-    pages[3].style.height = "0px";
-    pages[1].style.transition = "height 1s";
-    pages[2].style.transition = "height 1s";
-    pages[3].style.transition = "height 1s";
-    
+
+    // Get all pages
+    pages = document.getElementsByClassName ('tile_page');
+
+    for (var i = 0; i < pages.length; i++) {
+        pages[i].style.top = "100%";
+        pages[i].style.zIndex = i + 1;
+    }
+
+    pages[0].style.top = "0%";
+
     // Load code to type
     initializeTyperCode ();
-    
 }
 
 window.addEventListener('wheel', function(e) {
+    // Scrolled up
     if (e.deltaY < 0 && !muteWheelListener) {
-        console.log('scrolling up. Index: ' + pageIndex);
-        if (pageIndex != 1) {
-            pages [pageIndex].style.height = "0px";
-            pages [pageIndex - 1].style.height = screenHeight;
+        if (pageIndex != 0) {
+            pages [pageIndex].style.top = "100%";
             pageIndex--;
-            
+
             // Mute future mouse wheel events until the current window has finished expanding
             muteWheelListener = true;
             setTimeout (muteListener, 1000);
         }
+    // Scrolled down
     } else if (e.deltaY > 0 && !muteWheelListener) {
-        console.log('scrolling down. Index: ' + pageIndex);
-        if (pageIndex != 3) {
-            pages [pageIndex].style.height = "0px";
-            pages [pageIndex + 1].style.height = screenHeight;
+        if (pageIndex != (pages.length - 1)) {
+            pages [pageIndex + 1].style.top = "0%";
             pageIndex++;
-            
+
             // Mute future mouse wheel events until the current window has finished expanding
             muteWheelListener = true;
             setTimeout (muteListener, 1000);
